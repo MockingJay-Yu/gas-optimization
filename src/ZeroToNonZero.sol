87765b74fb0818f6340cd89bec0a777effe27d36
yudengke <yudengke1016@gmail.com>
2024-07-18 19:37:01 +0800

    inital project

---
+++ src/ZeroToNonZero.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+//dev When a storage variable changes from zero to non-zero,
+//it is more expensive than changing from non-zero to non-zero
+contract ZeroToNonZero {
+    uint256 public zero;
+    uint256 public one = 1;
+
+    //gas : 43368
+    function zeroToNonZero() public {
+        zero = 1;
+    }
+
+    //gas : 26224
+    function nonZeroToNonZero() public {
+        one = 2;
+    }
+
+    //gas : 21380
+    function nonZeroToZero() public {
+        one = 0;
+    }
+}
---
+++ test/ZeroToNonZeroTest.t.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import "forge-std/Test.sol";
+import "../src/ZeroToNonZero.sol";
+
+contract ZeroToNonZeroTest is Test {
+    ZeroToNonZero zeroToNonZero;
+
+    function setUp() public {
+        zeroToNonZero = new ZeroToNonZero();
+    }
+
+    function testZeroToNonZero() public {
+        zeroToNonZero.zeroToNonZero();
+        assertEq(zeroToNonZero.zero(), 1);
+    }
+
+    function testNonZeroToNonZero() public {
+        zeroToNonZero.nonZeroToNonZero();
+        assertEq(zeroToNonZero.one(), 2);
+    }
+
+    function testNonZeroToZero() public {
+        zeroToNonZero.nonZeroToZero();
+        assertEq(zeroToNonZero.one(), 0);
+    }
+}
