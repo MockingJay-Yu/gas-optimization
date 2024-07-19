313540c7987c502d841aecfcab81a13099201fdd
yudengke <yudengke1016@gmail.com>
2024-07-20 02:08:14 +0800

    modify comments

--- src/NeverUpdatedVariables.sol
+++ src/NeverUpdatedVariables.sol
@@ -2,9 +2,9 @@
 
 pragma solidity 0.8.20;
 
-//dev In Solidity, variables which are not intended to be updated should be constant or immutable.
+//dev In Solidity, variables which are not intended to be updated should be `constant` or `immutable`.
 //This is because `constant` and `immutable` values are embedded directly into the bytecode of the contract
-//which they are defined and does not use storage because of this.
+//which they are defined and does not use storage.
 //`constant` variables' value are determined at compile time and directly embedded into the bytecode.
 //`immutable` variables' value are determined at deployment time and embedded into the bytecode.
 contract Constants {
